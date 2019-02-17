8a87dced5cca41374a3bac85adcffb802747da7b
justinchan23 <justinchan23@gmail.com>
2019-02-16 20:51:57 -0800

    updated styling to adapt for various title & author name lengths

--- brainflix-sprint-i/src/Components/Video.js
+++ brainflix-sprint-i/src/Components/Video.js
@@ -6,7 +6,9 @@ class Video extends React.Component {
     return (
       <div className="asideSection__div">
         <img src={videoImage} className="asideSection__previewPic" alt="" />
-        <h3 className="asideSection__title">{videoTitle}</h3>
+        <div className="asideSection__titleDiv">
+          <h3 className="asideSection__title">{videoTitle}</h3>
+        </div>
         <h5 className="asideSection__name">{videoChannel}</h5>
       </div>
     )
