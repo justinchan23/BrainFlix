d6d49655066dfc51019dd5531ae85960852df9bf
justinchan23 <justinchan23@gmail.com>
2019-02-25 20:48:47 -0800

    update var to const

--- brainflix-sprint-iii/front-end/src/Components/Video.js
+++ brainflix-sprint-iii/front-end/src/Components/Video.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 const Video = props => {
   const { videoImage, videoTitle, videoChannel, videoId, currentVideo } = props
+  const link = '/videos/' + videoId
   var divId = ''
-  var link = '/videos/' + videoId
   if (videoId === currentVideo.id) {
     divId = 'currentVideo'
   }
