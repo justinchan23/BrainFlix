0dfa8ebe27ecb8ed14d34c105af6af14108c448d
justinchan23 <justinchan23@gmail.com>
2019-03-02 14:01:24 -0800

    update post method

--- brainflix-sprint-iii/back-end/routes/videoDetail.js
+++ brainflix-sprint-iii/back-end/routes/videoDetail.js
@@ -8,44 +8,48 @@ const videos = require('./database/videos')
 router.get('/', (req, res) => res.status(200).send(videos))
 
 router.post('/', (req, res) => {
-  const id = randomstring.generate({
-    length: 12,
-    charset: 'alphanumeric',
-    capitalization: 'lowercase'
-  })
+  if (res.body) {
+    const id = randomstring.generate({
+      length: 12,
+      charset: 'alphanumeric',
+      capitalization: 'lowercase'
+    })
 
-  const newVideo = {
-    id: id,
-    title: req.body.title,
-    channel: 'Justin Chan',
-    image: 'https://i.imgur.com/gUuqWIQ.jpg'
+    const newVideo = {
+      id: id,
+      title: req.body.title,
+      channel: 'Justin Chan',
+      image: 'https://i.imgur.com/gUuqWIQ.jpg'
+    }
+
+    const newVideoDetails = {
+      id: id,
+      title: req.body.title,
+      channel: 'Justin Chan',
+      image: 'https://i.imgur.com/gUuqWIQ.jpg',
+      description: req.body.description,
+      views: 0,
+      likes: 0,
+      duration: '1:23',
+      video: 'https://project-2-api.herokuapp.com/stream',
+      timestamp: Math.floor(new Date().getTime()),
+      comments: []
+    }
+
+    videos.push(newVideo)
+    videoDetails.push(newVideoDetails)
+
+    fs.writeFileSync('./routes/database/videos.json', JSON.stringify(videos))
+
+    fs.writeFileSync('./routes/database/videoDetails.json', JSON.stringify(videoDetails))
+
+    res.status(200).send({
+      success: true,
+      data: req.body
+    })
+  } else {
+    res.status(400).send({ message: 'Body required for post.' })
   }
-
-  const newVideoDetails = {
-    id: id,
-    title: req.body.title,
-    channel: 'Justin Chan',
-    image: 'https://i.imgur.com/gUuqWIQ.jpg',
-    description: req.body.description,
-    views: 0,
-    likes: 0,
-    duration: '1:23',
-    video: 'https://project-2-api.herokuapp.com/stream',
-    timestamp: Math.floor(new Date().getTime()),
-    comments: []
-  }
-
-  videos.push(newVideo)
-  videoDetails.push(newVideoDetails)
-
-  fs.writeFileSync('./routes/database/videos.json', JSON.stringify(videos))
-
-  fs.writeFileSync('./routes/database/videoDetails.json', JSON.stringify(videoDetails))
-
-  res.status(200).send({
-    success: true,
-    data: req.body
-  })
 })
 
 router.get('/:id', (req, res) => {
